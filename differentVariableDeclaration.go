6eb4876915ed3a8fdb2411751b8eff0903d18920
Geek <geek@praveensms007@gmail.com>
2018-07-27 21:31:38 +0530

    This is the example for creating a multiple variables in one diclaration

---
+++ differentVariableDeclaration.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+  var(
+    name = "Praveen"
+    age  = 19
+    height int
+  )
+
+  fmt.Println("My name is: ",name, "Age: ", age, "height: ", height)
+}
