063b8ce742f5a236205410a8ab69fa121c60fa04
Geek <geek@praveensms007@gmail.com>
2018-07-27 22:54:35 +0530

    This is the example for Short hand declaration. This will be more useful for declaring variables.

---
+++ shortHandDeclaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  a, b := 20, 30 // declare variables a and b
+  fmt.Println("a is", a, "b is", b)
+
+  name, age := "Praveen", 19
+  fmt.Println("My name is:",name, "Age is:",age)
+}
