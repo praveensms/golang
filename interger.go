a712f69696aa803862285c468a5fcfe6e3a37a4f
Geek <geek@praveensms007@gmail.com>
2018-07-29 11:08:51 +0530

    This is the example for different data types in the GoLang

---
+++ interger.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+  var num int8 = 124
+
+  var num2 int = 123
+  num4 := 1231.3
+
+  fmt.Println("Number is:", num, "Num2 is:",num2, "Num3 is:", num4, "Num4 type is: %T\n", num4)
+
+  fmt.Printf("Type of num4 is: %T\n", num4)
+  a, b := 5.67, 8.97
+    fmt.Printf("type of a %T b %T\n", a, b)
+}
